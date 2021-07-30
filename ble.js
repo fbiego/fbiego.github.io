4ec45de6b70b7facd9338555c8167a57bf08164d
Felix Biego <fbiego.fb@gmail.com>
2021-07-30 17:42:54 +0300

    web ble

---
+++ ble.js
@@ -0,0 +1,21 @@
+
+
+let scanButton = document.querySelector('#scanButton');
+let stopButton = document.querySelector('#stopButton');
+let scanAlert = document.querySelector('#outputText');
+
+
+
+function scanDevice(){
+	let options = {};
+	options.acceptAllDevices = true;
+
+	navigator.bluetooth.requestDevice(options)
+	.then(device => {
+		scanAlert.textContent'> Name:             ' + device.name + '\n> Id:               ' + device.id +'\n> Connected:        ' + device.gatt.connected;
+	})
+	.catch(error => {
+		scanAlert.textContent = 'Argh! ' + error;
+	});
+
+}
