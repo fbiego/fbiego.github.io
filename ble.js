42896d07518c0b5405d03d67f6bd75a5c0f1a71e
Felix Biego <fbiego.fb@gmail.com>
2021-07-30 18:24:43 +0300

    Update ble.js

--- ble.js
+++ ble.js
@@ -7,26 +7,46 @@ let scanAlert = document.querySelector('#outputText');
 
 
 async function scanDevice(){
-
-	scanAlert.textContent = 'Scanning...';
 	try {
 		scanAlert.textContent = 'Scanning...';
-		let options = {};
-		options.acceptAllDevices = true;
+		let options = {
+			acceptAllDevices : true
+		};
+		
 
-		const scan = await navigator.bluetooth.requestDevice(options)
+		const device = await navigator.bluetooth.requestDevice(options)
 		.then(device => {
 			scanAlert.textContent = 'Name: ' + device.name + ' Id: ' + device.id + ' Connected: ' + device.gatt.connected;
 		})
 		.catch(error => {
 			scanAlert.textContent = 'Argh! ' + error;
 		});
+		const server = await device.gatt.connect();
+		const services = await server.getPrimaryServices();
+		let sers = '';
+		for (const service of services) {
+			const characteristics = await service.getCharacteristics();
+			characteristics.forEach(characteristic => {
+				sers += characteristic.uuid + ' ' + getSupportedProperties(characteristic);
+			}
+		}
+		scanAlert.textContent = sers;
 	}
 	catch(error)  {
 		scanAlert.textContent = 'Argh! ' + error;
   	}
 
 
+}
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
 }
 
 scanButton.addEventListener('click', scanDevice);
