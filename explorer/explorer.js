bc5c99a28c78af30f9151bfa252bf5995ac826ea
Felix Biego <fbiego.fb@gmail.com>
2021-08-01 10:15:18 +0300

    Update explorer.js

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -6,6 +6,45 @@ let textAlert = document.querySelector('#outputText');
 let cardAlert = document.querySelector('#outputCard');
 
 
+
+async function loadPaired(){
+  try {
+    const devices = await navigator.bluetooth.getDevices();
+    removeAllChildNodes(deviceList);
+    for (const dev of devices){
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(dev.name));
+      li.setAttribute('class', 'w3-hover-blue');
+      li.addEventListener('click', async function(){
+        this.setAttribute('class', 'w3-pale-blue');
+        cardAlert.setAttribute('class', 'w3-container w3-card-4 w3-margin w3-pale-blue');
+        scanAlert.textContent = 'Connecting to ' + dev.name;
+        let abortController = new AbortController();
+        await dev.watchAdvertisements({signal: abortController.signal});
+        dev.addEventListener('advertisementreceived', async (evt) => {
+          abortController.abort();
+          connectDevice(dev);
+        });
+      }, false);
+      deviceList.appendChild(li);
+
+    }
+  }
+  catch{
+    cardAlert.setAttribute('class', 'w3-container w3-card-4 w3-margin w3-pale-red');
+    scanAlert.textContent = 'Argh! ' + error;
+  }
+}
+
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
 function myFunction(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
