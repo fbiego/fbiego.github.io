95fd69ddbc8f9bba6e7e03a9985a37676730cd9d
Felix Biego <fbiego.fb@gmail.com>
2021-08-01 14:10:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,118 +1,5 @@
 
 
-let scanButton = document.querySelector('#scanButton');
-let deviceList = document.querySelector('#deviceList');
-let textAlert = document.querySelector('#outputText');
-let cardAlert = document.querySelector('#outputCard');
-let serviceList = document.querySelector('#services');
-
-
-async function loadPaired(){
-  try {
-    const devices = await navigator.bluetooth.getDevices();
-    removeAllChildNodes(deviceList);
-    for (const dev of devices){
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(dev.name));
-      li.setAttribute('class', 'w3-hover-blue');
-      li.addEventListener('click', async function(){
-        this.setAttribute('class', 'w3-pale-blue');
-        cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
-        textAlert.textContent = 'Connecting to ' + dev.name;
-        let abortController = new AbortController();
-        await dev.watchAdvertisements({signal: abortController.signal});
-        dev.addEventListener('advertisementreceived', async (evt) => {
-          abortController.abort();
-          connectDevice(dev);
-        });
-      }, false);
-      deviceList.appendChild(li);
-
-    }
-  }
-  catch{
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = 'Argh! ' + error;
-  }
-}
-
-async function scanDevice(){
-  try {
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
-    textAlert.textContent = 'Scanning...';
-    let options = {
-      acceptAllDevices: true
-    };
-    
-
-    const device = await navigator.bluetooth.requestDevice(options);
-
-    connectDevice(device);
-    
-  }
-  catch(error)  {
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = 'Argh! ' + error;
-    }
-}
-
-async function loadServices(services){
-  try {
-    removeAllChildNodes(serviceList);
-    for (const service of services){
-      const s_uuid = service.uuid;
-      var button = document.createElement("button");
-      button.setAttribute('class', 'w3-button w3-block w3-blue w3-left-align');
-      button.setAttribute('onclick', 'myFunction(\''+s_uuid+'\')');
-      button.textContent = s_uuid;
-
-      var demoDiv = document.createElement('div');
-      demoDiv.setAttribute('id', s_uuid);
-      demoDiv.setAttribute('class', 'w3-hide w3-container');
-      var ul = document.createElement('ul');
-      ul.setAttribute('class', 'w3-ul');
-
-      serviceList.appendChild(button);
-      const characteristics = await service.getCharacteristics();
-      for (const ch of characteristics){
-        //srvs += ch.uuid + getSupportedProperties(ch) + "\r\n";
-        var li = document.createElement('li');
-        li.textContent = ch.uuid + ' ' + getSupportedProperties(ch);
-        ul.appendChild(li);
-      }
-      demoDiv.appendChild(ul);
-      serviceList.appendChild(demoDiv);
-
-    }
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-green');
-    textAlert.innerText = "";
-
-  }
-  catch (error){
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = 'Argh! ' + error;
-  }
-}
-
-async function connectDevice(device){
-
-  try {
-    const server = await device.gatt.connect();
-    const services = await server.getPrimaryServices();
-    // const main_service = await server.getPrimaryService(service_uuid);
-    // const tx_characteristic = await main_service.getCharacteristic(tx_uuid)
-    // const tx_value = await tx_characteristic.readValue();
-    // const str = new TextDecoder().decode(tx_value).split(",");
-
-    loadServices(services);
-  }
-  catch (error){
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = 'Argh! ' + error;
-  }
-
-}
-
 function getSupportedProperties(characteristic) {
   let supportedProperties = [];
   for (const p in characteristic.properties) {
@@ -123,7 +10,6 @@ function getSupportedProperties(characteristic) {
   return '[' + supportedProperties.join(', ') + ']';
 }
 
-scanButton.addEventListener('click', scanDevice);
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
