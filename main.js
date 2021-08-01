3ff44e04df47f2c161b519d255d4c41fbf5db18d
Felix Biego <fbiego.fb@gmail.com>
2021-08-01 14:22:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -30,12 +30,3 @@ function myFunction(id) {
   }
 }
 
-// Used to toggle the menu on smaller screens when clicking on the menu button
-function openNav() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
