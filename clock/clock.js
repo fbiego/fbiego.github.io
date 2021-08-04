39c847d7f778e33ffcf0e13145bd8ccee0e95037
Felix Biego <fbiego.fb@gmail.com>
2021-08-04 15:33:45 +0300

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -146,15 +146,17 @@ function setProperties(characteristic) {
       }
     }
   }
+  var span = document.createElement("span");
+  span.textContent = characteristic.uuid;
+  li.appendChild(span);
+  
   if (characteristic.properties.write || characteristic.properties.writeWithoutResponse){
     var input = document.createElement("input");
     input.setAttribute('class', 'w3-input w3-round w3-border');
     input.setAttribute('type', 'text');
     li.appendChild(input);
   }
-  var span = document.createElement("span");
-  span.textContent = characteristic.uuid;
-  li.appendChild(span);
+
   return li;
 }
 
