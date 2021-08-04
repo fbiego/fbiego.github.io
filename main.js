91ad582870ccc8cbcbae2dbf714a6435480a594e
Felix Biego <fbiego.fb@gmail.com>
2021-08-04 16:02:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ function myFunction(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-    x.previousElementSibling.className += " w3-theme-d1";
+    x.previousElementSibling.className += " w3-cyan";
   } else { 
     x.className = x.className.replace("w3-show", "");
     x.previousElementSibling.className = 
-    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+    x.previousElementSibling.className.replace(" w3-cyan", "");
   }
 }
 
