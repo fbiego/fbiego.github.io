2b641a4804dfcf0b9d94974c3b8960f03cf4e6c3
Felix Biego <fbiego.fb@gmail.com>
2021-08-04 16:27:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 let textCheck = document.querySelector('#checkText');
 let cardCheck = document.querySelector('#checkCard');
 
+const fromHexString = hexString =>
+  new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+const toHexString = bytes =>
+  bytes.reduce((str, byte) => str + byte.toString(16).padStart(5, ' 0x0'), '');
+
 
 async function checkBLE(){
   try {
