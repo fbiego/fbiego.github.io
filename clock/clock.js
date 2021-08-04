0a7f1cc0577de12e4f1b68c2f90da6efdba1b2ca
Felix Biego <fbiego.fb@gmail.com>
2021-08-04 17:20:34 +0300

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -78,35 +78,35 @@ async function scanDevice(){
     }
 }
 
-function connectClock(){
+// function connectClock(){
 
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
-    textAlert.textContent = 'Connecting...';
+//     cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
+//     textAlert.textContent = 'Connecting...';
     
-    navigator.bluetooth.requestDevice(options)
-    .then(device => device.gatt.connect())
-    .then(server => server.getPrimaryService(service_uuid))
-    .then(service => service.getCharacteristic(tx_uuid))
-    // .then(characteristic => characteristic.startNotifications())
-    // .then(characteristic => {
-    //   characteristic.addEventListener('characteristicvaluechanged', handleNotifications);
-    //   textAlert.textContent ="Notifications Enabled ";
-    // })
-    .then(characteristic => {
-      // Writing 1 is the signal to reset energy expended.
-      const resetEnergyExpended = Uint8Array.of(0xA0);
-      return characteristic.writeValueWithResponse(resetEnergyExpended);
-    })
-    .then(_ => {
-      cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
-    textAlert.textContent +="Done ";
-    })
-    .catch(error => { 
-      cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-      textAlert.textContent += error; 
-    });
+//     navigator.bluetooth.requestDevice(options)
+//     .then(device => device.gatt.connect())
+//     .then(server => server.getPrimaryService(service_uuid))
+//     .then(service => service.getCharacteristic(tx_uuid))
+//     // .then(characteristic => characteristic.startNotifications())
+//     // .then(characteristic => {
+//     //   characteristic.addEventListener('characteristicvaluechanged', handleNotifications);
+//     //   textAlert.textContent ="Notifications Enabled ";
+//     // })
+//     .then(characteristic => {
+//       // Writing 1 is the signal to reset energy expended.
+//       const resetEnergyExpended = Uint8Array.of(0xA0);
+//       return characteristic.writeValueWithResponse(resetEnergyExpended);
+//     })
+//     .then(_ => {
+//       cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
+//     textAlert.textContent +="Done ";
+//     })
+//     .catch(error => { 
+//       cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
+//       textAlert.textContent += error; 
+//     });
 
-}
+// }
 
 function handleNotifications(event){
   let value = event.target.value;
