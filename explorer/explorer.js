4d48b18fcc25b0999c2d42cbb08974d595a5477a
Felix Biego <fbiego.fb@gmail.com>
2021-08-07 13:06:45 +0300

    Update explorer.js

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -1,14 +1,25 @@
 
 
 let scanButton = document.querySelector('#scanButton');
+let disconnectButton = document.querySelector('#disconnectButton');
 let deviceList = document.querySelector('#deviceList');
 let textAlert = document.querySelector('#outputText');
 let cardAlert = document.querySelector('#outputCard');
 let serviceList = document.querySelector('#services');
-let serviceUUID = document.querySelector('#serviceUUID');
 let logs = document.querySelector('#notifyLogs');
 let read = false;
 
+let service_uuid = 'fb1e4001-54ae-4a28-9f74-dfccb248601d';
+let tx_uuid = 'fb1e4002-54ae-4a28-9f74-dfccb248601d';
+let rx_uuid = 'fb1e4003-54ae-4a28-9f74-dfccb248601d';
+
+let filters = [{namePrefix: 'ESP'}];
+
+let options = {
+  //acceptAllDevices: true,
+  optionalServices: [service_uuid],
+  filters : filters
+};
 
 async function loadPaired(){
   try {
@@ -43,18 +54,14 @@ async function scanDevice(){
   try {
     cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
     textAlert.textContent = 'Scanning...';
-    let options = {
-      acceptAllDevices: true
-      //, optionalServices: [serviceUUID.value]
-    };
+    
     
 
     const device = await navigator.bluetooth.requestDevice(options);
-
     
-    loadPaired();
     connectDevice(device);
-    
+
+
   }
   catch(error)  {
     cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
@@ -62,63 +69,52 @@ async function scanDevice(){
     }
 }
 
+// function connectClock(){
+
+//     cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
+//     textAlert.textContent = 'Connecting...';
+    
+//     navigator.bluetooth.requestDevice(options)
+//     .then(device => device.gatt.connect())
+//     .then(server => server.getPrimaryService(service_uuid))
+//     .then(service => service.getCharacteristic(tx_uuid))
+//     // .then(characteristic => characteristic.startNotifications())
+//     // .then(characteristic => {
+//     //   characteristic.addEventListener('characteristicvaluechanged', handleNotifications);
+//     //   textAlert.textContent ="Notifications Enabled ";
+//     // })
+//     .then(characteristic => {
+//       // Writing 1 is the signal to reset energy expended.
+//       const resetEnergyExpended = Uint8Array.of(0xA0);
+//       return characteristic.writeValueWithResponse(resetEnergyExpended);
+//     })
+//     .then(_ => {
+//       cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-blue');
+//     textAlert.textContent +="Done ";
+//     })
+//     .catch(error => { 
+//       cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
+//       textAlert.textContent += error; 
+//     });
+
+// }
+
 function handleNotifications(event){
   let value = event.target.value;
-  logs.innerText += '\n';
+  //const hex = toHexString(value);
+  logs.innerText += '\n' + event.target.uuid + ': ' ;
   for(let i = 0; i < value.byteLength; i++){
     logs.innerText += ' ' + value.getUint8(i).toString(16);
   }
 }
 
-async function loadServices(services){
-  try {
-    removeAllChildNodes(serviceList);
-    for (const service of services){
-      const s_uuid = service.uuid;
-      var button = document.createElement("button");
-      button.setAttribute('class', 'w3-button w3-block w3-blue w3-left-align');
-      button.setAttribute('onclick', 'myFunction(\''+s_uuid+'\')');
-      button.textContent = s_uuid;
-
-      var demoDiv = document.createElement('div');
-      demoDiv.setAttribute('id', s_uuid);
-      demoDiv.setAttribute('class', 'w3-hide w3-container');
-      var ul = document.createElement('ul');
-      ul.setAttribute('class', 'w3-ul');
-
-      serviceList.appendChild(button);
-      const characteristics = await service.getCharacteristics();
-      for (const ch of characteristics){
-        var li = setProperties(ch);
-        ul.appendChild(li);
-      }
-      demoDiv.appendChild(ul);
-      serviceList.appendChild(demoDiv);
-
-    }
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-green');
-    textAlert.innerText = "";
-
-  }
-  catch (error){
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = error;
-  }
-}
-
-async function connectDevice(device){
-
-  try {
-    const server = await device.gatt.connect();
-    const services = await server.getPrimaryServices();
-    loadServices(services);
-
-  }
-  catch (error){
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = error;
-  }
-
+function onDisconnected(event) {
+  removeAllChildNodes(serviceList);
+  disconnectButton.className += " w3-hide";
+  textAlert.textContent = 'Disconnected';
+  loadPaired();
+  // Object event.target is Bluetooth Device getting disconnected.
+  //log('> Bluetooth Device disconnected');
 }
 
 
@@ -207,5 +203,84 @@ function setProperties(characteristic) {
   return li;
 }
 
+
+async function loadServices(services){
+  try {
+    removeAllChildNodes(serviceList);
+    for (const service of services){
+      const s_uuid = service.uuid;
+      var button = document.createElement("button");
+      button.setAttribute('class', 'w3-button w3-block w3-blue w3-left-align');
+      button.setAttribute('onclick', 'myFunction(\''+s_uuid+'\')');
+      button.textContent = s_uuid;
+
+      var demoDiv = document.createElement('div');
+      demoDiv.setAttribute('id', s_uuid);
+      demoDiv.setAttribute('class', 'w3-hide w3-container');
+      var ul = document.createElement('ul');
+      ul.setAttribute('class', 'w3-ul');
+
+      serviceList.appendChild(button);
+      const characteristics = await service.getCharacteristics();
+      for (const ch of characteristics){
+        //srvs += ch.uuid + getSupportedProperties(ch) + "\r\n";
+        //var li = document.createElement('li');
+        // li.textContent = ch.uuid + ' ' + getSupportedProperties(ch);
+        // if (getSupportedProperties(ch).includes('NOTIFY', 0)){
+        //   ch.addEventListener('characteristicvaluechanged', handleNotifications);
+        //   //const desc = await ch.getDescriptors();
+        //   //li.innerText += '<br>Descriptors: ' + desc.map(c => c.uuid).join('\n' + ' '.repeat(19));
+
+          
+        // }
+        var li = setProperties(ch);
+        
+        ul.appendChild(li);
+      }
+      demoDiv.appendChild(ul);
+      serviceList.appendChild(demoDiv);
+
+    }
+    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-green');
+    textAlert.innerText = "";
+
+  }
+  catch (error){
+    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
+    textAlert.textContent = error;
+  }
+}
+
+async function connectDevice(device){
+
+  try {
+
+    device.addEventListener('gattserverdisconnected', onDisconnected);
+    const server = await device.gatt.connect();
+    const services = await server.getPrimaryServices();
+    loadServices(services);
+    disconnectButton.className = disconnectButton.className.replace(" w3-hide", "");
+    disconnectButton.addEventListener('click',  async (evt) => {
+      textAlert.textContent = 'Disconnecting...';
+      await device.gatt.disconnect();
+    });
+
+  }
+  catch (error){
+    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
+    textAlert.textContent = error;
+  }
+
+}
+
+function copyLogs(){
+  const text = logs.textContent;
+  navigator.clipboard.writeText(text);
+
+}
+
+function clearLogs(){
+  logs.textContent = "";
+}
 
 scanButton.addEventListener('click', scanDevice);
