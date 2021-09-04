0c659013a354a1ee619a99a56250df9205f750cc
Felix Biego <fbiego.fb@gmail.com>
2021-09-04 15:01:38 +0300

    Update clock.js

--- clock/clock.js
+++ clock/clock.js
@@ -161,6 +161,16 @@ function handleNotifications(event){
   }
   switch (value.getUint8(0)){
     case 0xB0: //
+      var tr = document.createElement("tr");
+      var td = document.createElement("td");
+      var td1 = document.createElement("td");
+      var date = value.getUint8(1) + " " + value.getUint8(2) + ":" + value.getUint8(3);
+      var bat = (value.getUint8(4) * 100) + + " " + value.getUint8(5);
+      td.innerText = date;
+      td1.innerText = bat;
+      tr.appendChild(td);
+      tr.appendChild(td1);
+      batteryTable.appendChild(tr);
 
     break;
   }
