3f2fe619d7ac868cced35b23eab4db0485499791
Felix Biego <fbiego.fb@gmail.com>
2021-09-06 13:13:34 +0300

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -98,6 +98,7 @@ function handleNotifications(event){
     	var total = (value.getUint8(4) * 256 * 256) + (value.getUint8(5) * 256) + value.getUint8(6);
 
     	var percent = (used/total) * 100;
+    	usageText.innerText = "" + used + "/" + total + " : " + percent + "%";
 
     break;
   }
