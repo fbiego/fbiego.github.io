cc20265a49acdcdbc61134e9a54e4d4c4d03902e
Felix Biego <fbiego.fb@gmail.com>
2021-09-06 11:16:50 +0300

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -105,41 +105,6 @@ function onDisconnected(event) {
 }
 
 
-async function loadServices(services){
-  try {
-    removeAllChildNodes(serviceList);
-    for (const service of services){
-      const s_uuid = service.uuid;
-      var button = document.createElement("button");
-      button.setAttribute('class', 'w3-button w3-block w3-blue w3-left-align');
-      button.setAttribute('onclick', 'myFunction(\''+s_uuid+'\')');
-      button.textContent = s_uuid;
-
-      var demoDiv = document.createElement('div');
-      demoDiv.setAttribute('id', s_uuid);
-      demoDiv.setAttribute('class', 'w3-hide w3-container');
-      var ul = document.createElement('ul');
-      ul.setAttribute('class', 'w3-ul');
-
-      serviceList.appendChild(button);
-      const characteristics = await service.getCharacteristics();
-      for (const ch of characteristics){
-        var li = setProperties(ch);
-        ul.appendChild(li);
-      }
-      demoDiv.appendChild(ul);
-      serviceList.appendChild(demoDiv);
-
-    }
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-green');
-    textAlert.innerText = "";
-
-  }
-  catch (error){
-    cardAlert.setAttribute('class', 'w3-container w3-margin w3-display-container w3-round w3-border w3-theme-border wl w3-pale-red');
-    textAlert.textContent = error;
-  }
-}
 
 async function connectDevice(device){
 
