1872150427ff7a27f4316243917dacba0ccb4ee8
Felix Biego <fbiego.fb@gmail.com>
2021-09-14 13:53:45 +0300

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -204,5 +204,22 @@ function clearLogs(){
   dataLogs.textContent = "";
 }
 
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+function saveData(){
+  // Start file download.
+  download(logName.textContent, dataLogs.textContent);
+}
 
 scanButton.addEventListener('click', scanDevice);
