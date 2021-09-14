ec9a0eb0f599d2364701db936d16a4f1fa38565a
Felix Biego <fbiego.fb@gmail.com>
2021-09-14 12:37:51 +0300

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -123,7 +123,8 @@ function handleNotifications(event){
     	down.setAttribute("class", "fa fa-download");
     	var span2 = document.createElement("span");
     	span2.setAttribute("class", "w3-button w3-round");
-    	span2.setAttribute("onclick", "sendCode('ba00"+toHexStr(dat) + "')");
+    	span.setAttribute("onclick", "sendCode('ba02"+toHexStr(dat) + "')");
+    	span2.setAttribute("onclick", "sendCode('ba01"+toHexStr(dat) + "')");
     	var del = document.createElement("i");
     	del.setAttribute("class", "fa fa-trash");
     	span.appendChild(del);
